491459e0447c020a5769c1e0cf684f98511a9356
daffanailmubarak07-cmyk <daffanailmubarak07@example.com>
2026-03-11 23:36:14 +0700

    void

--- bmi.cpp
+++ bmi.cpp
@@ -3,3 +3,10 @@
 
 using namespace std;
 
+// Prosedur untuk mengambil masukan data
+void masukanData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
